48a50770044435ade2d29f9eb171f00bdfebe57c
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-12-02 12:37:25 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -106,7 +106,7 @@ const Hero: React.FC = () => {
                 </div>
                 <div>
                   <div className="text-2xl font-bold text-eco-success">
-                    3.65 M+
+                    3.65 Million+
                   </div>
                   <div className="text-sm text-muted-foreground">
                     Litres Collected
